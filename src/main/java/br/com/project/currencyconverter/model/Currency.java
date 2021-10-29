ce001ed5a6e7d2bde440e682f006d95b349e228f
brcraver <brcraver@live.com>
2021-10-28 21:53:39 -0300

    Criacao Tabelas Banco Dados
    - 28/10/2021
    - Desenvolvimento
    - Criação da tabela Currency e da tabela TransactionHist no banco de dados.

---
+++ src/main/java/br/com/project/currencyconverter/model/Currency.java
@@ -0,0 +1,41 @@
+package br.com.project.currencyconverter.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import jdk.jfr.Name;
+import lombok.Data;
+
+@Entity
+@Table(name = "CURRENCY")
+@Data
+public class Currency {
+    
+
+    @Id
+    @Column(name = "id_usuario")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long idUsuario;
+
+    @Name("moeda_origem")
+    String moedaOrigem;
+
+    @Name("valor_origem")
+    Double valorOrigem;
+
+    @Name("moeda_destino")
+    String moedaDestino;
+
+    @Name("tx_conv_util")
+    Double txConvUtil;
+
+    @Name("data_hora")
+    Date dataHora;
+
+}
